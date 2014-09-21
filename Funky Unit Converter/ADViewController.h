ed5aeca386cbe81a919af9fbea024b2ec6251b7e
Igor Majer <CROiga@Igors-MacBook-Pro.local>
2014-09-21 21:45:52 +0200

    first revision

--- Funky Unit Converter/ADViewController.h
+++ Funky Unit Converter/ADViewController.h
@@ -9,5 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface ADViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *predjenihKmLabel;
+@property (strong, nonatomic) IBOutlet UILabel *potrosenihLitaraLabel;
+@property (strong, nonatomic) IBOutlet UILabel *potrosenihKnMotorLabel;
+@property (strong, nonatomic) IBOutlet UILabel *potrosenihKnAutobusLabel;
+@property (strong, nonatomic) IBOutlet UILabel *ukupnoPotrosenoLabel;
+@property (strong, nonatomic) IBOutlet UITextField *brojDanaTextField;
+@property (strong, nonatomic) IBOutlet UITextField *autobusnihKarataTextField;
+@property (strong, nonatomic) IBOutlet UITextField *pjeskeTextField;
+@property (strong, nonatomic) IBOutlet UITextField *prevezleKolegicaTextField;
+- (IBAction)izvrsiKalkulaciju:(UIButton *)sender;
 
 @end
--- Funky Unit Converter/ADViewController.m
+++ Funky Unit Converter/ADViewController.m
@@ -18,12 +18,44 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+   
 }
 
+- (IBAction)izvrsiKalkulaciju:(UIButton *)sender
+{
+    float brojDana = [self.brojDanaTextField.text floatValue];
+    float autobusnihKarata = [self.autobusnihKarataTextField.text floatValue];
+    float pjeske = [self.pjeskeTextField.text floatValue];
+    float prevezleKolegica = [self.prevezleKolegicaTextField.text floatValue];
+    
+    int voznjaMotorom = brojDana * 2 - autobusnihKarata - pjeske - prevezleKolegica;
+    int predjenihKm = voznjaMotorom * 9;
+    float potrosenihLitara = predjenihKm * 0.039;
+    float potrosenihKnM = potrosenihLitara * 11.3;
+    int potrosenihKnA = autobusnihKarata * 11;
+    float ukupnoPotroseno = potrosenihKnM + potrosenihKnA;
+    
+    self.predjenihKmLabel.text = [NSString stringWithFormat:@"%i" , predjenihKm];
+    self.potrosenihLitaraLabel.text = [NSString stringWithFormat:@"%f" , potrosenihLitara];
+    self.potrosenihKnMotorLabel.text = [NSString stringWithFormat:@"%f" , potrosenihKnM];
+    self.potrosenihKnAutobusLabel.text = [NSString stringWithFormat:@"%i" , potrosenihKnA];
+    self.ukupnoPotrosenoLabel.text = [NSString stringWithFormat:@"%f" , ukupnoPotroseno];
+    [self.prevezleKolegicaTextField resignFirstResponder];
+    [self.brojDanaTextField resignFirstResponder];
+    [self.autobusnihKarataTextField resignFirstResponder];
+    [self.pjeskeTextField resignFirstResponder];
+    
+    
+    
+    
+    
+    
+}
 @end
